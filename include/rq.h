ca6b0e334143b95e381d0b3e9749a7d94b0c9dbf
shinjiogaki <shinji.ogaki@gmail.com>
2015-04-19 18:17:28 +0900

    initial commit

---
+++ include/rq.h
@@ -0,0 +1,284 @@
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Redqueen Copyright (c) shinji All Rights Reserved.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef __rq_h__
+#define __rq_h__
+
+
+#define API __declspec(dllexport) 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Enumerations
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+enum Channel : int
+{
+	Beauty            = 1 <<  0,
+	LightPath         = 1 <<  1,
+	Lambertian        = 1 <<  2, // CameraAOV + LightAOV
+	LambertianColor   = 1 <<  3,
+	Transmissive      = 1 <<  4, // CameraAOV + LightAOV
+	TransmissiveColor = 1 <<  5,
+	Glossy            = 1 <<  6, // CameraAOV + LightAOV
+	GlossyColor       = 1 <<  7,
+	Specular          = 1 <<  8,
+	SpecularColor     = 1 <<  9,
+	Emissive          = 1 << 10,
+	Normal            = 1 << 11,
+	Tangent           = 1 << 12,
+	Position          = 1 << 13,
+	Depth             = 1 << 14,
+	UserData          = 1 << 15
+	// More Channels to come
+};
+
+
+enum class Element : char
+{
+	Lambertian, Transmissive, Glossy, Specular, Emissive,
+	Diffusion, // SSS
+	ShadowTransparency,
+	Normal,
+	Weight,
+	Anisotropy, Roughness, Angle, // Polishing
+	IOR, Fresnel, Thickness, // 
+	Clip
+};
+
+enum class AngleMeasure : char { Horizontal, Vertical, Diagonal }; // For Field of View
+enum class Sideness     : char { Face, Back, Both }; // 2 sided-material
+enum class Projection   : char { Perspective, Spherical, Orthographic }; // FOr camera and Gobo
+enum class Addressing   : char { Wrap, Clamp };
+enum class PixelSampler : char { Nearest, Stochastic, Bilinear };
+enum class Filter       : char { None, Gradient, Normal, Gaussian, Variance };
+enum class Path         : char { Null, Occlusion, DiffuseReflection, DiffuseTransmittance, GlossyReflection, GlossyRefraction, SpecularReflection, SpecularRefraction };
+enum class Material     : char { Metal, Glass, ThinGlass };
+enum class Visibility   : char { Visible, InvisibleFromCamera, InvisibleFromLight, Invisible };
+//enum class BRDF         : char { Ward };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Camera
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+API int  rqAddCamera            ( );
+API void rqSetCameraName        ( const int camera_id, const char*        name    );
+API void rqSetCameraProjection  ( const int camera_id, const Projection   type    );
+API void rqSetCameraAngleMeasure( const int camera_id, const AngleMeasure measure );
+API void rqSetCameraFOV         ( const int camera_id, const float        degree  );
+API void rqSetCameraBokeh       ( const int camera_id, const float        degree  );
+API void rqSetCameraTime        ( const int camera_id, const float        time    ); // time should be either 0.0f or 1.0f for now
+API void rqSetCameraSample      ( const int camera_id, const int          samples ); // AA Samples (can be an arbitrary number)
+
+API void rqSetCameraResolution  ( const int camera_id, const int width, const int height );
+API void rqSetCameraRegion      ( const int camera_id, const int left, const int top, const int right, const int bottom );
+
+API void rqSetCameraPosition    ( const int camera_id, const float x, const float y, const float z );
+API void rqSetCameraTarget      ( const int camera_id, const float x, const float y, const float z );
+API void rqSetCameraUpVector    ( const int camera_id, const float x, const float y, const float z );
+API void rqSetCameraExposure    ( const int camera_id, const float r, const float g, const float b );
+
+API int  rqAddCameraAOV         ( const int camera_id );
+API void rqSetCameraAOVName     ( const int camera_id, const int aov_id, const char*   name    );
+API void rqSetCameraAOVChannel  ( const int camera_id, const int aov_id, const Channel channel );
+API void rqSetCameraAOVUserData ( const int camera_id, const int aov_id, const char*   name    );
+
+API void rqInitializeCameras ( );
+API void rqFinalizeCameras   ( );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Light
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Point Light
+API int  rqAddPointLight           ( );
+API void rqSetPointLightTime       ( const int point_light_id, const float time );
+API void rqSetPointLightPosition   ( const int point_light_id, const float x, const float y, const float z );
+API void rqSetPointLightDirection  ( const int point_light_id, const float x, const float y, const float z );
+API void rqSetPointLightColor      ( const int point_light_id, const float r, const float g, const float b );
+API void rqSetPointLightPhoton     ( const int point_light_id, const int   photon     );
+API void rqSetPointLightSample     ( const int point_light_id, const int   sample     );
+API void rqSetPointLightBlur       ( const int point_light_id, const float blur       );
+API void rqSetPointLightOffset     ( const int point_light_id, const float offset     );
+API void rqSetPointLightInnerAngle ( const int point_light_id, const float inner_angle_degree );
+API void rqSetPointLightOuterAngle ( const int point_light_id, const float outer_angle_degree );
+API void rqSetPointLightShaderLink ( const int point_light_id, const int shader_id ); // Light Linking
+API int  rqAddPointLightAOV        ( const int point_light_id ); // Add AOV Channel
+API void rqSetPointLightAOVName    ( const int point_light_id, const int aov_id, const char*   name );
+API void rqSetPointLightAOVChannel ( const int point_light_id, const int aov_id, const Channel channel );
+
+
+// Parallel Light aka DIrectional Light
+API int  rqAddParallelLight           ( );
+API void rqSetParallelLightTime       ( const int parallel_light_id, const float time );
+API void rqSetParallelLightDirection  ( const int parallel_light_id, const float x, const float y, const float z );
+API void rqSetParallelLightColor      ( const int parallel_light_id, const float r, const float g, const float b );
+API void rqSetParallelLightPhoton     ( const int parallel_light_id, const int   photon );
+API void rqSetParallelLightSample     ( const int parallel_light_id, const int   sample );
+API void rqSetParallelLightBlur       ( const int parallel_light_id, const float blur   );
+API void rqSetParallelLightOffset     ( const int parallel_light_id, const float offset );
+API void rqSetParallelLightShaderLink ( const int parallel_light_id, const int shader_id ); // Light Linking
+API int  rqAddParallelLightAOV        ( const int parallel_light_id ); // Add AOV Channel
+API void rqSetParallelLightAOVName    ( const int parallel_light_id, const int aov_id, const char*   name );
+API void rqSetParallelLightAOVChannel ( const int parallel_light_id, const int aov_id, const Channel channel );
+
+
+// Gaffer Initializes all Non-Area Lights (Point Lights and Parallel Lights)
+API void rqInitializeGaffer( );
+API void rqFinalizeGaffer  ( );
+
+
+// Sky Light
+API void rqSetSkyLightTime       ( const float time   );
+API void rqSetSkyLightImage      ( const char* name   );
+API void rqSetSkyLightBackdrop   ( const char* name   );
+API void rqSetSkyLightPhoton     ( const int   photon );
+API void rqSetSkyLightSample     ( const int   sample );
+API void rqSetSkyLightColor      ( const float r, const float g, const float b );
+API void rqSetSkyLightNorth      ( const float x, const float y, const float z );
+API void rqSetSkyLightZenith     ( const float x, const float y, const float z );
+API void rqSetSkyLightShaderLink ( const int shader_id );
+API int  rqAddSkyLightAOV        ( );
+API void rqSetSkyLightAOVName    ( const int aov_id, const char*   name );
+API void rqSetSkyLightAOVChannel ( const int aov_id, const Channel channel );
+
+API void rqInitializeSkyLight  ( );
+API void rqFinalizeSkyLight    ( );
+
+
+// Geometry Light
+API void rqSetGeometryLightTime          ( const float time );
+API void rqSetGeometryLightPhoton        ( const int photon );
+API void rqSetGeometryLightSample        ( const int sample );
+API void rqSetGeometryLightShaderLink    ( const int shader_id );
+API int  rqAddGeometryLightAOV           ( );
+API void rqSetGeometryLightAOVName       ( const int aov_id, const char*   name    );
+API void rqSetGeometryLightAOVChannel    ( const int aov_id, const Channel channel );
+API void rqSetGeometryLightAOVShaderName ( const int aov_id, const char*   shader  );
+
+API void rqInitializeGeometryLight ( );
+API void rqFinalizeGeometryLight   ( );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Rendering
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+API void rqSetRendererSample     ( const int   sample     );
+API void rqSetRendererBounce     ( const int   bounce     );
+API void rqSetRendererRadius     ( const float radius     );
+API void rqSetRendererResolution ( const float resolution );
+API void rqSetRendererDistance   ( const float distance   );
+API void rqSetRendererRayDepth   ( const int   depth      );
+API void rqSetRendererPhotonDepth( const int   depth      );
+API void rqSetRendererClamp      ( const float r, const float g, const float b );
+
+API void rqInitializeRenderer  ( );
+API void rqFinalizeRenderer    ( );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Display
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+API void rqSetDisplayGamma  ( const float gamma   );
+API void rqSetPreviewWindow ( const bool  preview );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Shader
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+API int  rqAddShader                ( );
+API void rqSetShaderName            ( const int shader_id, const char* name );
+API void rqSetShaderTime            ( const int shader_id, const float time );
+API void rqSetShaderSmoothAngle     ( const int shader_id, const float smooth_angle           );
+API void rqSetShaderRoundCorner     ( const int shader_id, const float round_corner           );
+API void rqSetShaderHairUV          ( const int shader_id, const bool  assign_hair_texture_uv );
+
+API int  rqAddSurfaceShader         ( const int shader_id );
+API void rqSetSurfaceSideness       ( const int shader_id, const int layer_id, const Sideness   sideness   );										   
+API void rqSetSurfaceMaterial       ( const int shader_id, const int layer_id, const Material   material   );
+API void rqSetSurfaceVisibility     ( const int shader_id, const int layer_id, const Visibility visibility );
+API void rqSetSurfaceVirtualLight   ( const int shader_id, const int layer_id, const bool       virtual_light );
+API void rqSetSurfaceColor          ( const int shader_id, const int layer_id, const Element    element, const float r, const float g, const float b ); // Set Color or Values
+API void rqSetSurfaceImage          ( const int shader_id, const int layer_id, const Element    element, const char* name, const float gamma, const float color_matrix[ 4 ][ 4 ], const float uv_matrix[ 3 ][ 3 ] );
+
+// Displacement Map (Needs Tangents and Normals to Work. They can be created by setting a proper smoothing angle )
+API int  rqAddDisplacementShader    ( const int shader_id );
+API void rqSetDisplacementLevel     ( const int shader_id, const int displacement_id, const int level ); // Subdivision Level
+API void rqSetDisplacementVector    ( const int shader_id, const int displacement_id, const float x, const float y, const float z );
+API void rqSetDisplacementImage     ( const int shader_id, const int displacement_id, const char* name, const float gamma, const float color_matrix[ 4 ][ 4 ], const float uv_matrix[ 3 ][ 3 ] );
+
+// Volume Shader
+//API int  rqAddVolumeShader        ( const int shader_id );
+//API void rqSetVolumeTransmittance ( const int shader_id, const float r, const float g, const float b ); // Transmittance Per Meter
+//API void rqSetVolumeAlbedo        ( const int shader_id, const float r, const float g, const float b ); // Decay Per Particle Collision
+//API void rqSetVolumeAverageCosine ( const int shader_id, const float cosine );                          // Phase Function
+
+// Shader Library
+API void rqInitializeShaders ( );
+API void rqFinalizeShaders   ( );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Geometry
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+API int  rqAddObject     ( );
+API int  rqAddObject     ( const char* name );
+API void rqSetObjectName ( const int object_id, const char* name );
+API void rqSetObjectTime ( const int object_id, const float time );
+
+// Group
+API int  rqAddGroup     ( const int object_id );
+
+// Set Temporal Shader ID
+API void rqSetShaderID ( const int object_id, const int group_id, const int shader_id );
+
+// Vertex Data
+API void rqAddRadii    ( const int object_id, const int group_id, const int n, const float* data );
+API void rqAddUVs      ( const int object_id, const int group_id, const int n, const float* data );
+API void rqAddPositions( const int object_id, const int group_id, const int n, const float* data );
+API void rqAddNormals  ( const int object_id, const int group_id, const int n, const float* data );
+API void rqAddTangents ( const int object_id, const int group_id, const int n, const float* data );
+
+// Primitive
+API void rqAddParticles ( const int object_id, const int group_id, const int n, const int* vertex_ids );
+API void rqAddCylinders ( const int object_id, const int group_id, const int n, const int* vertex_ids );
+API void rqAddTriangles ( const int object_id, const int group_id, const int n, const int* vertex_ids );
+API void rqAddTetragons ( const int object_id, const int group_id, const int n, const int* vertex_ids );
+
+// Multi-Level Instance
+API int  rqAddInstance ( const int object_id );
+API void rqSetInstance ( const int object_id, const int instance_id, int instanced_object_id, float matrix[4][4] );
+
+// Per Vertex/Primitive data "position", "uv", and "radius" are reserved.
+API void rqAddVertexData    ( const int object_id, const int group_id, const char* name, const int n, const int dim, const float* data ); // The length of data is n x 1 s s .....
+//API void rqAddPrimitiveData ( const int object_id, const int group_id, const char* name, const int n, const int dim, const float* data ); // The length of data is n x 1 s s .....
+
+// Mesh Processing to Come..
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Accelerator
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+API void rqInitializeAccelerator( );
+API void rqFinalizeAccelerator  ( );
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Utility
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+API void rqStartup      ( );
+API void rqInitialize   ( ); // Initialize everything in an appropriate Order
+API void rqFinalize     ( ); // Finalize   everything in an appropriate Order
+API void rqShutdown     ( );
+//API void rqSerialize    ( const char* folder_name ); // Serialize   Scene
+//API void rqDeserialize  ( const char* folder_name ); // Deserialize Scene
+API void rqGo           ( const char* folder_name ); // This function does everything for you
+API void rqRender       ( );
+API void rqRenderPixels ( const int num_of_pixels, float* rgb_s, const int subpixel_id );
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,324 @@
+
+// Select your favorite One
+
+//#define CLASSIC
+#define MY_APP
+
+
+// Include API header
+#include "rq.h"
+
+#include <iostream>
+
+
+
+// Code to Build redqueen.exe
+#ifdef CLASSIC
+int main( int argc, char *argv[ ] )
+{
+	if( 2 <= argc )
+	{
+		for( auto i = 2; i < argc; ++i )
+		{
+			if( 0 == strcmp( argv[ i ], "-preview" ) ) { rqSetPreviewWindow( true ); }
+		}
+		rqGo( argv[ 1 ] );
+	}
+	return 0;
+}
+#endif
+
+
+
+// Code to Build Your Own Software
+#ifdef MY_APP
+int main( )
+{
+
+	////////////////////////
+	// Startup redqueen
+	////////////////////////
+	rqStartup( );
+
+
+	////////////////////////
+	// Camera
+	////////////////////////
+	auto camera_id = rqAddCamera( );
+	{
+		rqSetCameraSample      ( camera_id, 31 ); // AA samples can be an arbitrary number
+		rqSetCameraRegion      ( camera_id, 16, 16, 496, 496 ); // Render Region
+		rqSetCameraResolution  ( camera_id, 512, 512 );
+		rqSetCameraProjection  ( camera_id, Projection  ::Perspective );
+		rqSetCameraAngleMeasure( camera_id, AngleMeasure::Vertical    );
+
+		// Camera Pose - Shutter Open
+		rqSetCameraTime     ( camera_id,  0.0f );
+		rqSetCameraFOV      ( camera_id, 40.0f );
+		rqSetCameraBokeh    ( camera_id,  0.5f );
+		rqSetCameraPosition ( camera_id, -1, 1, -4 );
+		rqSetCameraTarget   ( camera_id, 0, 1,  0 );
+		rqSetCameraUpVector ( camera_id, 0, 1,  0 );
+
+		// Camera Pose - Shutter Close
+		rqSetCameraTime     ( camera_id,  1.0f );
+		rqSetCameraFOV      ( camera_id, 40.0f );
+		rqSetCameraBokeh    ( camera_id,  0.5f );
+		rqSetCameraPosition ( camera_id, -1, 1, -4 );
+		rqSetCameraTarget   ( camera_id, 0, 1,  0 );
+		rqSetCameraUpVector ( camera_id, 0, 1,  0 );
+		//rqSetCameraUpVector ( camera_id, 0.2, 1,  0 ); // Camera Blur :)
+
+		// AOVs
+		auto aov_id0 = rqAddCameraAOV( camera_id );
+		auto aov_id1 = rqAddCameraAOV( camera_id );
+		auto aov_id2 = rqAddCameraAOV( camera_id );
+		rqSetCameraAOVName     ( camera_id, aov_id0,     "normal.png" );
+		rqSetCameraAOVName     ( camera_id, aov_id1, "lambertian.png" );
+		rqSetCameraAOVName     ( camera_id, aov_id2,    "my_data.png" );	
+		rqSetCameraAOVChannel  ( camera_id, aov_id0, Channel::Normal     );
+		rqSetCameraAOVChannel  ( camera_id, aov_id1, Channel::Lambertian );
+		rqSetCameraAOVChannel  ( camera_id, aov_id2, Channel::UserData   );
+		rqSetCameraAOVUserData ( camera_id, aov_id2, "my_data" );
+	}
+
+
+	////////////////////////
+	// Shader
+	////////////////////////
+	auto shader_id0 = rqAddShader( );
+	auto shader_id1 = rqAddShader( );
+	{
+		float color_matrix[4][4] =
+		{
+			1,0,0,0,
+			0,1,0,0,
+			0,0,1,0,
+			0,0,0,1
+		};
+	
+		float uv_matrix[3][3] =
+		{
+			2,0,0,
+			0,2,0,
+			0,0,1
+		};
+	
+		auto surface_shader_id0 = rqAddSurfaceShader( shader_id0 );
+		rqSetShaderName   ( shader_id0, "white" );
+		rqSetSurfaceColor ( shader_id0, surface_shader_id0, Element::Lambertian, 0.8f, 0.8f, 0.8f );
+		//rqSetSurfaceImage ( shader_id0, surface_shader_id0, Element::Emissive, "C:\\Shinji\\redqueen\\light.hdr", 1.0f, 0, 0 ); // null : identity matrix
+	
+		auto surface_shader_id1 = rqAddSurfaceShader( shader_id1 );
+		rqSetShaderName   ( shader_id1, "glossy");
+		rqSetSurfaceColor ( shader_id1, surface_shader_id1, Element::Lambertian, 0.9f, 0.9f, 0.9f );
+		rqSetSurfaceColor ( shader_id1, surface_shader_id1, Element::Glossy    , 1.0f, 1.0f, 1.0f );
+		rqSetSurfaceColor ( shader_id1, surface_shader_id1, Element::Roughness , 0.2f, 0.2f, 0.2f );
+		rqSetSurfaceColor ( shader_id1, surface_shader_id1, Element::IOR       , 5.0f, 5.0f, 5.0f ); // For Fresnel
+		//rqSetSurfaceImage ( shader_id1, surface_shader_id1, Element::Glossy,  "C:\\Shinji\\redqueen\\light.hdr", 1.0f, 0, 0 ); // null : identity matrix
+		
+		/*
+		// Displacement Map Example
+		{
+			rqSetShaderSmoothAngle( shader_id0, 180 );
+		
+			rqAddDisplacementShader ( shader_id0 );
+			rqSetDisplacementVector ( shader_id0, 0, 0, 0, -0.1 );
+			//rqSetDisplacementImage  ( shader_id0, 0, "C:\\Shinji\\redqueen\\stone.jpg", 0.2f, 0, 0 );
+			rqSetDisplacementLevel  ( shader_id0, 0, 16 );
+								    
+			rqAddDisplacementShader ( shader_id0 );
+			rqSetDisplacementVector ( shader_id0, 1, 0, 0, 0.01 );
+			//rqSetDisplacementImage  ( shader_id0, 1, "C:\\Shinji\\redqueen\\stone.jpg", 0.2f, 0, 0 );
+			rqSetDisplacementLevel  ( shader_id0, 1, 4 );
+		}
+		*/
+
+	}	
+
+	////////////////////////
+	// Geometry
+	////////////////////////
+
+	auto instanced_object_id = rqAddObject( );
+	{
+
+		// Particles
+		rqSetObjectName( instanced_object_id, "particles" );
+		{
+			// Add Group
+			auto group_id = rqAddGroup ( instanced_object_id );
+
+			// xyzxyz...
+			float positions[ ] = { 0.3, 0.2, 0, -0.3, 0.2, 0 };
+			float radii    [ ] = { 0.2, 0.2 };
+			int   ids      [ ] = { 0, 1   };
+			rqSetShaderID ( instanced_object_id, group_id, shader_id0  );
+			rqAddPositions( instanced_object_id, group_id, 2, positions );
+			rqAddRadii    ( instanced_object_id, group_id, 2, radii );
+			rqAddParticles( instanced_object_id, group_id, 2, ids );
+		}
+
+	}
+
+
+	// Add Object
+	auto object_id = rqAddObject( );
+	{
+
+		
+		// Instance
+		{
+			float matrix0[4][4] = {1,0,0,0,   0,1,0,0,     0,0,1,0,   0,0,0,1  };
+			float matrix1[4][4] = {1,0,0,0,   0,1,0,0.5,   0,0,1,0,   0,0,0,1  };
+			auto instance_id0 = rqAddInstance( object_id );
+			auto instance_id1 = rqAddInstance( object_id );
+			rqSetInstance( object_id, instance_id0, instanced_object_id, matrix0 );
+			rqSetInstance( object_id, instance_id1, instanced_object_id, matrix1 );
+		}
+		
+
+		// Add Group
+		auto group_id = rqAddGroup ( object_id );
+
+		// Trianlges
+		{
+			// xyzxyz...
+			float positions[ ] =
+			{
+				-1, 0,  1, -1, 0, -1, 1, 0, -1, 1, 0,  1,
+				-1, 2,  1, -1, 2, -1, 1, 2, -1, 1, 2,  1,
+				-1, 2,  1, -1, 0,  1, 1, 0,  1, 1, 2,  1
+			};
+
+			// uvuvuvuv...
+			float uvs[ ] =
+			{
+				0, 1, 0, 0, 1, 0, 1, 1,
+				0, 1, 0, 0, 1, 0, 1, 1,
+				0, 1, 0, 0, 1, 0, 1, 1
+			};
+
+			// rgbrgb...
+			float my_data[ ] =
+			{
+				1,0,0, 1,0,0, 1,0,0, 1,0,0,
+				0,1,0, 0,1,0, 0,1,0, 0,1,0,
+				0,0,1, 0,0,1, 0,0,1, 0,0,1
+			};
+
+			rqAddPositions ( object_id, group_id, 12, positions );
+			rqAddUVs       ( object_id, group_id, 12, uvs       );
+			rqAddVertexData( object_id, group_id, "my_data", 12, 3, my_data );
+
+			{
+				int vertex_ids[ ] = { 0, 1, 2, 0, 2, 3 };
+				rqSetShaderID  ( object_id, group_id,    shader_id0 );
+				rqAddTriangles ( object_id, group_id, 2, vertex_ids );
+			}
+			
+			{
+				int vertex_ids[ ] = { 4, 5, 6, 7, 8, 9, 10, 11 };	
+				rqSetShaderID  ( object_id, group_id,    shader_id1 );
+				rqAddTetragons ( object_id, group_id, 2, vertex_ids );
+			}
+
+		}
+
+		
+		// Cylinder (Ribbon) Example
+		/*
+		{
+			// Add Group
+			auto group_id = rqAddGroup ( object_id );
+
+			// xyzxyz...
+			float positions[ ] = { -1, -1, 0, 0, 0,  0, 1, 1,  0 };
+			float radii    [ ] = { 0.05, 0.025, 0.0 };
+			int   ids      [ ] = { 0, 1  };
+			rqSetShaderID ( object_id, group_id, shader_id1 );
+			rqAddPositions( object_id, group_id, 3, positions );
+			rqAddRadii    ( object_id, group_id, 3, radii );
+			rqAddCylinders( object_id, group_id, 2, ids);
+		}
+		*/
+
+	}
+
+
+
+	////////////////////////
+	// Light
+	////////////////////////
+	
+	{
+		/*
+		auto point_light_id = rqAddPointLight( );
+		{
+			rqSetPointLightPosition  ( point_light_id, 2.5, 3.0, 2.5 );
+			rqSetPointLightColor     ( point_light_id,  20,  2,  2 );
+			rqSetPointLightDirection ( point_light_id,  -1,  -1,  -1 );
+			rqSetPointLightInnerAngle( point_light_id,  0 );
+			rqSetPointLightOuterAngle( point_light_id,  5 );
+		}
+		*/
+	
+		auto parallel_light_id = rqAddParallelLight( );
+		{
+			rqSetParallelLightDirection( parallel_light_id, 1, -1, 1 );
+			rqSetParallelLightColor    ( parallel_light_id, 0.5, 0.5, 0.5 );
+			rqSetParallelLightPhoton   ( parallel_light_id, 1000000 );
+
+			// Light AOVs
+			auto aov_id = rqAddParallelLightAOV ( parallel_light_id );
+			rqSetParallelLightAOVName    ( parallel_light_id, aov_id, "parallel_light_lambertian.hdr");
+			rqSetParallelLightAOVChannel ( parallel_light_id, aov_id, Channel::Lambertian );
+		}
+	}
+
+
+
+	////////////////////////
+	// Skylight
+	////////////////////////
+	{
+		rqSetSkyLightColor  ( 0.5,0.5,1 );
+		rqSetSkyLightZenith ( 0,1,0 );
+		rqSetSkyLightNorth  ( 0,0,-1 );
+		rqSetSkyLightSample ( 64 );
+		rqSetSkyLightPhoton (1000000);
+		//rqSetSkyLightImage ( "C:\\Shinji\\redqueen\\image.hdr" );
+		
+		// Light AOVs
+		auto aov_id = rqAddSkyLightAOV ( );
+		rqSetSkyLightAOVName    ( aov_id, "sky_lambertian.hdr");
+		rqSetSkyLightAOVChannel ( aov_id, Channel::Lambertian );
+	}
+
+
+	////////////////////////
+	// Display
+	////////////////////////
+	rqSetPreviewWindow ( true );
+
+
+	////////////////////////
+	// Rendering
+	////////////////////////
+	rqSetRendererSample( 256 );
+	rqSetRendererBounce( 0 );
+	rqSetRendererResolution( 0.1 );
+	//rqSetRendererDistance ( 0 );
+	//rqSetRendererClamp( 1,1,1 );
+
+	rqInitialize ( );
+	rqRender     ( );
+	rqFinalize   ( );
+
+	// Shutdown redqueen
+	rqShutdown( );
+
+	return 0;
+
+}
+#endif
